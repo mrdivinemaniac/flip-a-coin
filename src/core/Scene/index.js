010a9b5fd1eaad0835f494ada4f4cdd4b2e76f41
mrdivinemaniac <divinemaniac@gmail.com>
2019-05-20 00:39:22 +0545

    Maintain active game objects in Scene object

--- src/core/Scene/index.js
+++ src/core/Scene/index.js
@@ -3,18 +3,32 @@ class Scene {
     const { gl } = context
     this.context = context
     this.__clearColor = [1.0, 1.0, 1.0, 1.0]
+    this.__gameObjects = []
     gl.enable(gl.DEPTH_TEST)
     gl.enable(gl.CULL_FACE)
     gl.frontFace(gl.CCW)
     gl.cullFace(gl.BACK)
+
+    this.__drawGameObject = this.__drawGameObject.bind(this)
   }
 
   setClearColor (clearColor) {
+    this.context.gl.clearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3])
     this.__clearColor = clearColor
   }
 
   addGameObject (gameObject) {
+    const { __gameObjects } = this
     gameObject.setScene(this)
+    gameObject.__sceneIndex = __gameObjects.length
+    __gameObjects.push(gameObject)
+  }
+
+  removeGameObject (gameObject) {
+    const index = gameObject.__sceneIndex
+    if (index !== undefined) {
+      this.__gameObjects.splice(index, 1)
+    }
   }
 
   setCamera (camera) {
@@ -33,10 +47,14 @@ class Scene {
     return this.context.canvas.width
   }
 
+  __drawGameObject (gameObject) {
+    gameObject.draw(this.__camera.viewMat, this.__camera.projMat)
+  }
+
   draw () {
-    const { __clearColor, context: { gl } } = this
-    gl.clearColor(__clearColor[0], __clearColor[1], __clearColor[2], __clearColor[3])
+    const { context: { gl } } = this
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    this.__gameObjects.forEach(this.__drawGameObject)
   }
 }
 
